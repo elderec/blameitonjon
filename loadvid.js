39899748f8abb270886aae9c2304efa8f1164e32
elderec <elderec@users.noreply.github.com>
2024-03-25 11:16:34 -0700

    use localstorage
    Track last seen video using localstorage to prevent duplicate video on page refresh.

--- loadvid.js
+++ loadvid.js
@@ -1,29 +1,36 @@
-    <!-- Load the video(s) -->
-      // Constants: Number of videos and DOM elements for video and source tags.
-      const VIDEO_COUNT = 34;
-      const videoElement = document.getElementById('clip');
-      const mp4Source = document.getElementById('mp4');
-      const webmSource = document.getElementById('webm');
+// Constants: Number of videos and DOM elements for video and source tags.
+const VIDEO_COUNT = 34;
+const videoElement = document.getElementById('clip');
+const mp4Source = document.getElementById('mp4');
+const webmSource = document.getElementById('webm');
 
-      // Function to generate a random integer between min and max, inclusive.
-      // Used to select a random video index.
-      function getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-      }
+// Function to generate a random integer between min and max, inclusive.
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-      // Function to load a random video.
-      // It selects a random index and updates the source elements' src attributes.
-      function loadRandomVideo() {
-        const videoIndex = getRandomInt(1, VIDEO_COUNT);
-        mp4Source.src = `images/${videoIndex}.mp4`;
-        webmSource.src = `images/${videoIndex}.webm`;
-        videoElement.load();  // Loads the new video source into the video element.
-      }
+// Function to load a random video that hasn't been played last.
+function loadRandomVideo() {
+  let lastVideoIndex = parseInt(localStorage.getItem('lastVideoIndex'));
+  let videoIndex;
 
-      // Initialize the page by loading a random video.
-      loadRandomVideo();
+  // Ensure we pick a different video if there's a last video index saved
+  do {
+    videoIndex = getRandomInt(1, VIDEO_COUNT);
+  } while (videoIndex === lastVideoIndex);
 
-      // Set attributes to disable remote playback and picture-in-picture
-      // to maintain consistent user experience across different browsers and devices.
-      videoElement.setAttribute('disableRemotePlayback', '');
-      videoElement.setAttribute('disablePictureInPicture', '');
+  // Update the video sources
+  mp4Source.src = `images/${videoIndex}.mp4`;
+  webmSource.src = `images/${videoIndex}.webm`;
+  
+  // Load the new video source into the video element and save the index
+  videoElement.load();
+  localStorage.setItem('lastVideoIndex', videoIndex.toString());
+}
+
+// Initialize the page by loading a random video.
+loadRandomVideo();
+
+// Set attributes to maintain consistent user experience.
+videoElement.setAttribute('disableRemotePlayback', '');
+videoElement.setAttribute('disablePictureInPicture', '');
